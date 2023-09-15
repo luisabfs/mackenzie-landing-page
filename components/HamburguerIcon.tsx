7e8c475b863870a8eadf554f4129d536c7678f34
Luisa Barros <luisa.barros.dev@gmail.com>
2023-09-15 17:23:41 -0300

    feat: create hamburguer menu icon

---
+++ components/HamburguerIcon.tsx
@@ -0,0 +1,9 @@
+export function HamburgerIcon () {
+  return(
+    <div className='p-1/2'>
+      <svg className="w-8 h-8 fill-white visile lg:hidden" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" viewBox="0 0 24 24" stroke="currentColor">
+        <path d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </div>
+  );
+}
