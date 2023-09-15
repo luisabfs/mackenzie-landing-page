7d74aafa8702935779f4343db5921778554788a5
Luisa Barros <luisa.barros.dev@gmail.com>
2023-09-15 11:47:31 -0300

    feat: configure fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Bebas_Neue } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ variable: '--font-inter', subsets: ['latin'] })
+const bebas = Bebas_Neue({ variable: '--font-bebas-neue', subsets: ['latin'], weight: ['400'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className={`${bebas.variable} ${inter.variable} font-sans`} lang="en">
+      <body>{children}</body>
     </html>
   )
 }
